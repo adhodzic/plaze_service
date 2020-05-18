9fb882ecf09a3825efd83f39d237f9fd842ccaab
Adnan Hodžić <adhodzic@unipu.hr>
2020-05-18 19:24:01 +0200

    'heroku'

--- src/server.js
+++ src/server.js
@@ -327,5 +327,9 @@ app.get('/comments/:id', async(req, res, next) => {
     res.status(200).json(comments)
 })
 const port = process.env.PORT || 3000;
+if(process.env.NODE_ENV === 'production') {
+    app.use(express.static(__dirname + '/public/'));
 
+    app.get(/.*/, (req, res) => res.sendFile(__dirname + '/public/index.html'))
+}
 app.listen(port, () => (console.log(`Listening on port ${port}`)))
